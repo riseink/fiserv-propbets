e3c056ae46ac52f2ef8161de9c775a0b31ffeeb4
Seth Epstein <sepstein@sepstein-mbp.lan>
2021-01-09 16:37:18 -0600

    gate keep funtionality

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Link } from "gatsby"
-import UserForm from '../components/userform'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import firebase from "../components/firebase"
 
+const db = firebase.firestore();
+
+db.collection('players').get().then(snapshot => {
+  snapshot.docs.forEach(doc => {
+      const Allplayers = doc.data();
+    })
+})
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Fiserv Prop Bets</h1>
     <p>Let's Get started</p>
-    <UserForm />
   </Layout>
 )
-
-
 
 export default IndexPage
