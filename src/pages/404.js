09f77e8125d25ce878e82080f6c3db670537aa6d
Seth Epstein <sepstein@sepstein-mbp.lan>
2021-01-10 07:19:21 -0600

    Linting

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,7 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import firebase from "../components/firebase"
 
-const db = firebase.firestore();
-
-db.collection('players').get().then(snapshot => {
-  snapshot.docs.forEach(doc => {
-      const Allplayers = doc.data();
-    })
-})
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
