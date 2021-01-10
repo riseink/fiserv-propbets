a82e974ebc7f86a0682fe6ee2510153c44d56d90
Seth Epstein <sepstein@sepstein-mbp.lan>
2021-01-10 09:28:18 -0600

    more debug

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,19 +2,11 @@ import React from "react";
 // import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import firebase from "../components/firebase"
 
-const db = firebase.firestore();
-
-db.collection('players').get().then(snapshot => {
-  snapshot.docs.forEach(doc => {
-      console.log(doc.data())
-    })
-})
 
 const HomePage = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="Home" />
     <h1>Fiserv Prop Bets</h1>
     <p>Let's Get started</p>
   </Layout>
