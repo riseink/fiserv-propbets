ae766d8d870339dd6be89324e3bcdd1804ec2ede
Seth Epstein <sepstein@sepstein-mbp.lan>
2021-01-13 16:05:03 -0600

    base table styles

--- src/components/header_alt.js
+++ src/components/header_alt.js
@@ -4,43 +4,43 @@ import React from "react"
 
 const HeaderAlt = ({ siteTitle }) => (
   <header className="alt_nav"
-    
+
   >
 
 
-          <div className="logo">
-          <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          <img className="img_responsive" alt="chevron" src="/nav_logo_alt.png" />
-          </Link>
-          </div>
-       
+    <div className="logo">
+      <Link
+        to="/"
+        style={{
+          color: `white`,
+          textDecoration: `none`,
+        }}
+      >
+        <img className="img_responsive" alt="chevron" src="/nav_logo_alt.png" />
+      </Link>
+    </div>
+
 
     <div className="nav_links">
       <ul>
         <li>
-        <Link
-          to="/home/#overview">overview</Link>
+          <Link
+            to="/home/#overview">overview</Link>
         </li>
 
         <li>
-        <Link
-          to="/home/#gameplay">gameplay</Link>
+          <Link
+            to="/home/#gameplay">gameplay</Link>
         </li>
 
         <li className="active">
-        <Link
-          to="#">place bets</Link>
+          <Link
+            to="#">place bets</Link>
         </li>
 
         <li>
-        <Link
-          to="/leaderboard">leaderboard</Link>
+          <Link
+            to="/leaderboard">leaderboard</Link>
         </li>
       </ul>
     </div>
--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import firebase from "../components/firebase";
+import Helmet from 'react-helmet'
 
 const db = firebase.firestore();
 
@@ -23,78 +24,90 @@ class LeaderBoard extends React.Component {
 
   render() {
     const { players } = this.state;
-    return (
-      <table style={{width: 'calc(100% - 80px)', margin: '40px auto'}}>
-        <tr>
-          <th>Question</th>
-          {players.map((player) => (
-            <th>
-              {player.player_first_name} {player.player_last_name}
-            </th>
-          ))}
-        </tr>
-        <tr>
-          <td>How long will it take to sing the national anthem?</td>
-          {players.map((player) => (
-            <td>{player.question1}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>Length of the word "Brave" during the anthem?</td>
-          {players.map((player) => (
-            <td>{player.question2}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>What will the result of the coin toss be?</td>
-          {players.map((player) => (
-            <td>{player.question3}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>Who will win the coin toss?</td>
-          {players.map((player) => (
-            <td>{player.question4}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>Which coach will broadcasters mention first after kickoff?</td>
-          {players.map((player) => (
-            <td>{player.question5}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>First commercial spot after the 1st quarter will feature what brand?</td>
-          {players.map((player) => (
-            <td>{player.question6}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>How many commercials will feature dogs?</td>
-          {players.map((player) => (
-            <td>{player.question7}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>What beer brand's commercial will appear first?</td>
-          {players.map((player) => (
-            <td>{player.question8}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>Will the president be referenced by the broadcasters at any point?</td>
-          {players.map((player) => (
-            <td>{player.question9}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>How many times will they show Pat Mahomes' family on the broadcast?</td>
-          {players.map((player) => (
-            <td>{player.question10}</td>
-          ))}
-        </tr>
 
-      </table>
+    return (
+
+      <div className="table-wrapper">
+        <Helmet>
+          <body class='body_table' />
+        </Helmet>
+        <table className="fl-table" cellSpacing="0 20px">
+          <thead>
+            <tr>
+              <th>Question</th>
+              {players.map((player) => (
+                <th>
+                  {player.player_first_name} {player.player_last_name}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>How long will it take to sing the national anthem?</td>
+              {players.map((player) => (
+                <td>{player.question1}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>Length of the word "Brave" during the anthem?</td>
+              {players.map((player) => (
+                <td>{player.question2}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>What will the result of the coin toss be?</td>
+              {players.map((player) => (
+                <td>{player.question3}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>Who will win the coin toss?</td>
+              {players.map((player) => (
+                <td>{player.question4}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>Which coach will broadcasters mention first after kickoff?</td>
+              {players.map((player) => (
+                <td>{player.question5}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>First commercial spot after the 1st quarter will feature what brand?</td>
+              {players.map((player) => (
+                <td>{player.question6}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>How many commercials will feature dogs?</td>
+              {players.map((player) => (
+                <td>{player.question7}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>What beer brand's commercial will appear first?</td>
+              {players.map((player) => (
+                <td>{player.question8}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>Will the president be referenced by the broadcasters at any point?</td>
+              {players.map((player) => (
+                <td>{player.question9}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>How many times will they show Pat Mahomes' family on the broadcast?</td>
+              {players.map((player) => (
+                <td>{player.question10}</td>
+              ))}
+            </tr>
+          </tbody>
+
+        </table>
+      </div>
+
     );
   }
 }
