100507bb9b6284be49d6969eef47f1e6f167b4e8
Seth Epstein <sepstein@sepstein-mbp.lan>
2021-01-27 09:56:05 -0600

    iframe

--- src/pages/video.js
+++ src/pages/video.js
@@ -14,7 +14,8 @@ const VideoPage = () => (
   <video controls autoplay="true">
     <source src="http://www.intersporthub.com/wp-content/uploads/2020/02/19ADIDAS_FNS_FINAL_720_29_FOR_HUB.mp4?id=0" />
   
-  </video>    
+  </video>
+  <iframe width="560" title="unique" height="315" src="http://www.intersporthub.com/wp-content/uploads/2020/02/19ADIDAS_FNS_FINAL_720_29_FOR_HUB.mp4?id=0" frameborder="0" allowfullscreen></iframe>
 </div>
   </LayoutAlt>
 )
